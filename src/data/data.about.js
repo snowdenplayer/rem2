d80a7a5f06d2d64b2cac08f2b9ca6434a923cf84
snowden <rebitson20@gmail.com>
2021-02-20 11:32:00 +0200

    deploy

--- src/data/data.about.js
+++
@@ -1,9 +0,0 @@
-export const aboutData = [
-    require('../assets/images/stairs.jpg'),
-    require('../assets/images/vana2.jpg'),
-    require('../assets/images/vana3.jpg'),
-    require('../assets/images/stairs.jpg'),
-    require('../assets/images/vana2.jpg'),
-    require('../assets/images/vana3.jpg'),
-
-]
