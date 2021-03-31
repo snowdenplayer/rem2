671aad6a5eb8cba52de06a4b2b85cf7c5d2bdff4
snowden <rebitson20@gmail.com>
2021-03-31 14:46:54 +0300

    add slider

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,9 @@
           <div class="our__logo">Ремонтна <br> компанія</div>
           <div class="our__text">Робимо Ваші оселі комфортними<br>більше 3-х років</div>
 
-          <div class="our__button"><router-link to="/about" class="our__link">Про нас</router-link></div>
+          <div class="our__button">
+            <router-link to="/about" class="our__link">Про нас</router-link>
+          </div>
         </div>
       </div>
     </section>
@@ -14,56 +16,74 @@
       <div class="experience__container container">
         <h1 class="experience__header container__header">Наші переваги</h1>
         <div class="experience__boxes">
-          <div class="box box-white"><div class="num">30+</div>зданих <br> проєктів </div>
+          <div class="box box-white">
+            <div class="num">30+</div>
+            зданих <br> проєктів
+          </div>
           <div class="box box-yellow">Лише<br>якісні<br>інструменти</div>
           <div class="box box-dark">Працюємо<br>з<br>гарантією</div>
-          <div class="box box-gray"><div class="num">3+</div> роки <br> досвіду</div>
+          <div class="box box-gray">
+            <div class="num">3+</div>
+            роки <br> досвіду
+          </div>
         </div>
       </div>
-
+    </section>
+    <section class="slider">
+      <div class="slider__container container">
+        <sliderFeedBacks></sliderFeedBacks>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
+import sliderFeedBacks from "../components/sliderFeedbacks"
 
 export default {
   name: 'Home',
   components: {
+    sliderFeedBacks
   }
 }
 </script>
 
 <style scoped lang="scss">
 @import "../assets/styles/index";
-.our{
+
+.our {
   padding-top: 100px;
   background-image: url("../assets/images/sectionONas.jpg");
   background-color: #f0f0f0;
   background-size: 65% 100vh;
   background-position: right;
   background-repeat: no-repeat;
-  height: 100vh;//480px
+  height: 100vh; //480px
   display: flex;
   justify-content: center;
   box-sizing: border-box;
-  &__container{
+
+  &__container {
     display: flex;
     flex-direction: column;
     justify-content: center;
   }
-  &__logo{
+
+  &__logo {
     font-family: arial;
     font-size: 60px;
   }
-  &__text{
+
+  &__text {
     margin-top: 20px;
     font-size: 19px;
     color: #8e8e8e;
   }
-  &__button{
+
+  &__button {
     margin-top: 30px;
-    .our__link{
+
+    .our__link {
       display: flex;
       justify-content: center;
       align-items: center;
@@ -77,38 +97,47 @@ export default {
     }
   }
 }
-@media screen and (max-width: 1024px){
-  .our{
+
+@media screen and (max-width: 1024px) {
+  .our {
     position: relative;
-    &::after{
+
+    &::after {
       content: "";
       position: absolute;
       top: 0;
       left: 0;
       width: 100%;
       height: 100%;
-      background-color:rgba(0, 0, 0, 0.5);
+      background-color: rgba(0, 0, 0, 0.5);
       z-index: 2;
     }
+
     background-size: cover;
-    &__container{
+
+    &__container {
       align-items: flex-start;
     }
-    .wrap__info{
+
+    .wrap__info {
       z-index: 3;
     }
-    &__logo{
+
+    &__logo {
       z-index: 3;
       color: #fff;
       text-align: left;
     }
-    &__text{
+
+    &__text {
       color: #fff;
       z-index: 3;
     }
-    &__button{
+
+    &__button {
       z-index: 3;
-      .our__link{
+
+      .our__link {
         color: #fff;
         border-color: #fec41d;
 
@@ -116,24 +145,28 @@ export default {
     }
   }
 }
-@media screen and (max-width: 640px){
-  .our{
-    &__container{
+
+@media screen and (max-width: 640px) {
+  .our {
+    &__container {
       align-items: center;
     }
-    &__logo{
+
+    &__logo {
       text-align: left;
     }
-    &__button{
-      .our__link{
+
+    &__button {
+      .our__link {
         width: 85%;
       }
     }
   }
 }
+
 /*end our section*/
 /*experience start*/
-.experience{
+.experience {
   height: 600px;
   background-color: #f0f0f0;
   background-size: 35% 100%;
@@ -143,21 +176,25 @@ export default {
   justify-content: center;
 
   box-sizing: border-box;
-  &__header{
+
+  &__header {
     margin-bottom: -50px;
   }
-  &__container{
+
+  &__container {
     display: flex;
     flex-direction: column;
     align-items: flex-end;
     justify-content: space-around;
   }
-  &__boxes{
+
+  &__boxes {
     display: flex;
     flex-wrap: wrap;
     width: 400px;
     border: 5px solid white;
-    .box{
+
+    .box {
       box-sizing: border-box;
       width: 200px;
       height: 200px;
@@ -169,21 +206,26 @@ export default {
       font-size: 25px;
       font-weight: bold;
       text-align: center;
-      .num{
+
+      .num {
         font-size: 35px;
       }
-      &-yellow{
+
+      &-yellow {
         background-color: #fec41d;
         color: #fff;
       }
-      &-dark{
+
+      &-dark {
         background-color: #252527;
         color: #fff;
       }
-      &-gray{
+
+      &-gray {
         background-color: #f0f0f0;
       }
-      &-white{
+
+      &-white {
         background-color: #fff;
       }
     }
@@ -192,44 +234,53 @@ export default {
 
 
 }
-@media screen and (max-width: 1024px){
-  .experience{
+
+@media screen and (max-width: 1024px) {
+  .experience {
     background: url("../assets/images/experienceLong.jpeg");
     background-size: cover;
     background-position: center;
-    &__container{
+
+    &__container {
       align-items: center;
+
     }
   }
 }
-@media screen and (max-width: 640px){
-  .experience{
+
+@media screen and (max-width: 640px) {
+  .experience {
     height: 600px;
     position: relative;
-    &::after{
+
+    &::after {
       content: "";
       position: absolute;
       top: 0;
       left: 0;
       width: 100%;
       height: 100%;
-      background-color:rgba(0, 0, 0, 0.5);
+      background-color: rgba(0, 0, 0, 0.5);
       z-index: 2;
     }
-    &__container{
+
+    &__container {
       align-items: center;
       z-index: 3;
     }
-    &__header{
+
+    &__header {
       color: #fff;
 
       margin-bottom: 0px;
     }
-    &__boxes{
+
+    &__boxes {
       flex-direction: column;
       border: none;
       width: 90%;
-      .box{
+
+      .box {
         width: 100%;
         height: 130px;
       }
@@ -237,7 +288,15 @@ export default {
     }
   }
 }
+
 /*end experience*/
+.slider{
+  display: flex;
+  justify-content: center;
+  &__container{
+    padding: 20px 0;
+  }
+}
 
 
 </style>
