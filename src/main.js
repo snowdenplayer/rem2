3ed0428b5399b1ae22349685257f27026400bd36
snowdenTFTC <Nahurnyi.Vladyslav@tftc.dev>
2021-11-28 23:11:18 +0200

    feat new branch

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/styles/reset.scss'
-import './assets/styles/index.scss'
-import 'material-design-icons-iconfont'
-
-createApp(App).use(router).mount('#app')
